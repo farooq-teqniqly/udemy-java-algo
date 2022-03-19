215dd3ea7d97117edc1e989c2572d8037475a7d4
farooq <farooq@teqniqly.com>
2022-03-19 12:32:17 -0700

    Graph with adjacency list implementation.

--- Graphs/src/GraphApp.java
+++ Graphs/src/GraphApp.java
@@ -1,6 +1,6 @@
 public class GraphApp {
     public static void main(String[] args) {
-        Graph graph = new Graph(true);
+        Graph graph = new Graph(false);
         Vertex a = new Vertex("A");
         Vertex b = new Vertex("B");
         Vertex c = new Vertex("C");
@@ -14,7 +14,11 @@ public class GraphApp {
 
         graph.showNeighbors(neighbors -> {
             for (Vertex neighbor : neighbors) {
-                System.out.print(neighbor.getKey() + " -> ");
+                if (neighbor == null) {
+                    continue;
+                }
+
+                System.out.print(neighbor.getKey() + ", ");
             }
 
             System.out.println();
