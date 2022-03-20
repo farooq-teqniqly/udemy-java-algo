807f044b8515824e57ae255d58b326b22c22d9eb
Farooq <farooq@teqniqly.com>
2022-03-19 23:34:27 -0700

    BFS implementation.

--- Graphs/src/MovieApp.java
+++ Graphs/src/MovieApp.java
@@ -6,12 +6,24 @@ import java.util.Scanner;
 public class MovieApp {
     public static void main(String[] args) throws FileNotFoundException {
         String fileName = "C:\\src\\my\\uwf-cop6416\\project\\py\\data\\processed\\movie_casts.csv";
-        MovieFileProcessor processor = new MovieFileProcessor();
-        List<CastMember> castMembers = processor.process(new Scanner(new File(fileName)));
 
-        System.out.println("Created " + castMembers.size() + " movie records.");
+        System.out.println("Processing file " + fileName);
+
+        MovieFileProcessor processor = new MovieFileProcessor();
+
+        long start = System.currentTimeMillis();
+        List<CastMember> castMembers = processor.process(new Scanner(new File(fileName)));
+        long end = System.currentTimeMillis();
+
+        System.out.println("Created " +
+                castMembers.size() +
+                " movie records in " +
+                (end - start) +
+                " ms.");
 
         Graph graph = new Graph(false);
+
+        start = System.currentTimeMillis();
 
         for (CastMember castMember : castMembers) {
             graph.add(
@@ -19,14 +31,35 @@ public class MovieApp {
                     new ActorVertex(castMember.getActorId().trim(), castMember.getActor().trim()));
         }
 
+        end = System.currentTimeMillis();
+
+        System.out.println("Created a graph of " +
+                graph.getAdjacencyMap().size() +
+                " verticies in "
+                + (end - start) +
+                " ms.");
+
         BFSSearcher searcher = new BFSSearcher(graph);
 
         Vertex source = graph.getAdjacencyMap().get("nm0000102").get(0);
         Vertex target = graph.getAdjacencyMap().get("nm0000199").get(0);
 
-        searcher.Execute(source);
+        System.out.println("Running BFS.");
 
+        start = System.currentTimeMillis();
+        searcher.Execute(source);
+        end = System.currentTimeMillis();
+
+        System.out.println("BFS ran in " + (end - start) + " ms.");
+
+        System.out.println("Getting shortest path.");
+
+        start = System.currentTimeMillis();
         List<Vertex> path = searcher.path(source, target);
+        end = System.currentTimeMillis();
+
+        System.out.println("Got shortest path in " + (end - start) + " ms.");
+
         int degrees = -1;
 
         for (Vertex v : path) {
